bf2be8c7fd8be495550002d06cc850160a1333c4
Russell Brand <brand@responsible.com>
2025-02-01 15:28:45 -0800

    convert to CSS

--- second/src/main.js
+++ second/src/main.js
@@ -16,19 +16,6 @@ for (let i = 1; i < DAY_LENGTH; i += 1) {
   console.log(i);
   const line = document.createElement("div");
   line.className = "hour_block";
-
-  line.style.display = "flex";
-  line.style.alignItems = "center";
-  // line.style.justifyContent = "center";
-  line.style.textIndent = "-100%";
-  // line.style.color = "black";
-  line.style.fontSize = "12px";
-  line.style.position = "absolute";
-  line.style.left = "10%";
-  line.style.width = "90%";
-  line.style.height = "2px";
-  line.style.backgroundColor = "orange";
-  line.style.position = "absolute";
   line.style.top = `${i * delta}px`;
   line.textContent = (i + START_HOUR) % 12 || 12;
   document.querySelector("#timeline_block").appendChild(line);
