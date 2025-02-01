205d95d5a9a5987e5c2b5d70c2082bbd06e0d3ea
Russell Brand <brand@responsible.com>
2025-02-01 15:45:07 -0800

    refactor

--- second/src/main.js
+++ second/src/main.js
@@ -1,24 +1,36 @@
 import "./style.css";
 import { setupCounter } from "./counter.js";
 
-var START_HOUR = 9;
+var START_HOUR = 6;
 var END_HOUR = 22;
 var DAY_LENGTH = END_HOUR - START_HOUR;
+
+function calculateHour(hour) {
+  return hour % 12 || 12;
+}
+
+function createHourBlock(hour, topPosition) {
+  const line = document.createElement("div");
+  line.className = "hour_block";
+  line.style.top = `${topPosition}px`;
+  line.textContent = hour;
+  return line;
+}
 
 document.querySelector("#app").innerHTML = `
   <div>
       <button id="counter" type="button"></button>
+      
       <div id="timeline_block"></div>
   </div>
 `;
 var delta = document.querySelector("#timeline_block").offsetHeight / DAY_LENGTH;
 for (let i = 1; i < DAY_LENGTH; i += 1) {
   console.log(i);
-  const line = document.createElement("div");
-  line.className = "hour_block";
-  line.style.top = `${i * delta}px`;
-  line.textContent = (i + START_HOUR) % 12 || 12;
-  document.querySelector("#timeline_block").appendChild(line);
+  const hour = calculateHour(i + START_HOUR);
+  const topPosition = i * delta;
+  const hourBlock = createHourBlock(hour, topPosition);
+  document.querySelector("#timeline_block").appendChild(hourBlock);
 }
 
 setupCounter(document.querySelector("#counter"));
