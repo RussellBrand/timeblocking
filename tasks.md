07c1df2a944f2fd40c45628459c2e10351315808
Russell Brand <brand@responsible.com>
2025-02-02 07:41:59 -0800

    Simple display in REACT

--- tasks.md
+++ tasks.md
@@ -1,8 +1,38 @@
 # tasks
+## react
+### [y] typedefs
+Typedef for Status -- enum of OPEN DONE SKIPPED
+
+Typedef for ID -- positive integer
+
+Typedef for TIME -- float 
+
+Typedef for DURATION -- float 
+
+Typedef for Activity 
+
+id: ID
+title: string
+status: status
+start: TIME
+duration: DURATION
 
 
-## [ ] give each actity and ID
-## [ ] if we put the day start & end on #timeline block, 
+ [
+    createActivity("Morning Exercise", 6.00, 1,1001),
+    createActivity("Work on Project", 9.00, 3, 1002),
+    createActivity("Lunch Break", 12.00, 1, 1003),
+    createActivity("Team Meeting", 14.00, 2, 1004),
+    createActivity("Evening Walk", 18.00, 1, 1005),
+  ];
+
+
+
+
+
+## original 
+### [ ] give each actity and ID
+### [ ] if we put the day start & end on #timeline block, 
 and factor out the computations of start time and duration height, 
 we could do panning an zooming by changing the day start & end on #timeline block, 
 
@@ -10,29 +40,29 @@ not a necessarily a wonderful permanent solution, but could work
 
 it almost makes we want to do this in react...
 
-## [ ] 
-## [ ] 
+### [ ] 
+### [ ] 
 
-## [ ] emacs support for triple hash
-## [Y] console ninja
-## [Y] time based bounding box
-## [ ] save/restore to local storage
-## [ ] save/restore to local disk
-## [ ] display at the right verical "time" point
-## [ ] display with duration as "item height"
-## [Y] create activity
-## [ ] edit time
-## [ ] edit length
-## [ ] edit text
-## [ ] display state
-## [ ] edit state
-## [ ] second column
-## [Y] drag & drop
-## [ ] collisions
-## [ ] pick a framework
-## [ ] typescript
-## [ ] jsx
-## [ ] recalculate times for drag & drop
-## [ ] figure out the long-click, short-click issue
-## [ ] 
-## [ ] 
+### [ ] emacs support for triple hash
+### [Y] console ninja
+### [Y] time based bounding box
+### [ ] save/restore to local storage
+### [ ] save/restore to local disk
+### [ ] display at the right verical "time" point
+### [ ] display with duration as "item height"
+### [Y] create activity
+### [ ] edit time
+### [ ] edit length
+### [ ] edit text
+### [ ] display state
+### [ ] edit state
+### [ ] second column
+### [Y] drag & drop
+### [ ] collisions
+### [ ] pick a framework
+### [ ] typescript
+### [ ] jsx
+### [ ] recalculate times for drag & drop
+### [ ] figure out the long-click, short-click issue
+### [ ] 
+### [ ] 
--- tbbt-rt/src/App.tsx
+++ tbbt-rt/src/App.tsx
@@ -1,28 +1,137 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// Types
 
+enum Status {
+  OPEN = "open",
+  DONE = "done",
+  SKIPPED = "skipped",
+}
+
+// DURATION is a positive number representing the number of hours
+type DURATION = number & { __type: "DURATION" };
+
+function createDuration(value: number): DURATION {
+  if (value <= 0) {
+    throw new Error("DURATION must be a positive number");
+  }
+  return value as DURATION;
+}
+
+type ID = number & { __type: "ID" };
+
+function createID(value?: number): ID {
+  if (value === undefined) {
+    const RANDOM_ID_MAX = 1_000_000;
+    value = Math.floor(Math.random() * RANDOM_ID_MAX) + 1; // Generate a random positive integer
+  }
+  if (value <= 0 || !Number.isInteger(value)) {
+    throw new Error("ID must be a positive integer");
+  }
+  return value as ID;
+}
+
+// number of hours since midnight
+type TIME = number & { __type: "TIME" };
+
+function createTime(value: number): TIME {
+  if (value < 0) {
+    throw new Error("TIME must be a non-negative number");
+  }
+  return value as TIME;
+}
+
+const NON_BREAKING_SPACE = "\u00A0";
+
+function formatTime(time: TIME | number): string {
+  const hours = Math.floor(time);
+  const minutes = Math.round((time - hours) * 60);
+  return `${hours.toString().padStart(2, NON_BREAKING_SPACE)}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+type Activity = {
+  id: ID;
+  title: string;
+  status: Status;
+  start: TIME;
+  duration: DURATION;
+};
+
+function createActivity(
+  title: string,
+  start: TIME | number,
+  duration: DURATION | number,
+  { id, status }: { id?: ID | number; status?: Status } = {}
+): Activity {
+  return {
+    id: createID((id as number) || undefined),
+    title,
+    status: status || Status.OPEN,
+    start: createTime(start),
+    duration: createDuration(duration),
+  };
+}
+
+function ignore_unused<T>(_: T) {
+  return _;
+}
+
+function App() {
+  const [activities, _setActivities] = useState<Activity[]>([
+    createActivity("Divide App.tsx into seperate files", 6.0, 1, { id: 1001 }),
+    createActivity("create typedefs", 9.0, 3, {
+      id: 1002,
+      status: Status.DONE,
+    }),
+    createActivity("Make form for creating new tasks", 12.0, 1, { id: 1003 }),
+    createActivity("Reimplement in COBOL", 14.0, 2, {
+      id: 1004,
+      status: Status.SKIPPED,
+    }),
+    createActivity("Make activities editable", 18.0, 1, { id: 1005 }),
+    createActivity("Sort activities by time", 2.0, 3),
+    createActivity("Display on a timeline", 12.5, 0.5),
+    createActivity(
+      "make a lint rule for leading underscore without making the STATUS enum generter spurious warnings -- then we can remove the ignore_unused function",
+      13.0,
+      1
+    ),
+  ]);
+  ignore_unused(_setActivities);
   return (
     <>
       <h1>Time Blocking</h1>
       <h2>React Typescript</h2>
       <div>Form for creating new tasks goes here</div>
-      <div>Container for columns goes here</div>
-
-      <div className="card">
-        <button
-          onClick={() => {
-            console.log("count", count);
-            setCount((count) => count + 10);
-          }}
-        >
-          count is {count}
-        </button>
-      </div>
+      <DisplayActivities activities={activities} />
     </>
   );
 }
 
 export default App;
+
+function DisplayActivities({ activities }: { activities: Activity[] }) {
+  return (
+    <div>
+      <h2>{activities.length} Activities</h2>
+      <ul>
+        {activities.map((activity) => (
+          <ActivityItem key={activity.id} activity={activity} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function ActivityItem({ activity }: { activity: Activity }) {
+  return (
+    <li className={`activity ${activity.status.toLowerCase()}`}>
+      {formatTime(activity.start)} --{" "}
+      {formatTime(activity.start + activity.duration)} -- ({activity.id}){" "}
+      {activity.title} -- {activity.status}
+    </li>
+  );
+}
